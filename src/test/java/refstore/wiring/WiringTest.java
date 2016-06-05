07f5650a86a9209740851031a6956c0da5618708
Jimmy Petersen <jimmy.petersen@defiant.dk>
2016-06-05 10:19:46 +0200

    Add tests and coverage format in HTML,XML

---
+++ src/test/java/refstore/wiring/WiringTest.java
@@ -0,0 +1,50 @@
+package refstore.wiring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WiringTest {
+
+	public static class get {
+		@Test(expected = NullPointerException.class)
+		public void failsOnNullId() {
+			Wiring.get(null);
+		}
+		
+		@Test
+		public void returnsRegistryOfCorrectClass() {
+			assertTrue(Wiring.get("myId") instanceof Wiring);
+		}
+		
+		@Test
+		public void returnsRegistryWithCorrectId() {
+			Object id = new Object();
+			assertEquals(id, Wiring.get(id).getId());
+		}
+		
+		@Test
+		public void reusesExistingRegistry() {
+			Object id       = new Object();
+			Wiring expected = Wiring.get(id);
+			Wiring actual   = Wiring.get(id);
+			assertEquals(expected, actual);
+		}
+	}
+	
+	public static class getDefault {
+		@Test
+		public void returnsRegistryWithNonNullId() {
+			assertNotNull(Wiring.getDefault());
+		}
+		
+		@Test
+		public void reusesExistingRepository() {
+			Wiring expected = Wiring.getDefault();
+			Wiring actual   = Wiring.getDefault();
+			assertEquals(expected, actual);
+		}
+	}
+	
+ 
+}
