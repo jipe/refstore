022e7db05e1bb91c7551e5741ffbe5908d8329c2
Jimmy Petersen <jimmy.petersen@defiant.dk>
2016-10-15 10:23:46 +0200

    Include container start/stop in maven lifecycle
    - Use exec-maven-plugin for starting and stopping containers
    - Update container scripts to return useful error codes

--- start_containers.sh
+++ start_containers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker_cmd='docker-compose up --build -d'
 cmd="sudo $docker_cmd"
+wget=$(which wget)
 
 for g in $(groups); do
   if [ $g == 'docker' ]; then
@@ -9,4 +10,20 @@ for g in $(groups); do
 done
 
 cd docker
-exec $cmd 2>&1
+$cmd
+docker_return=$?
+
+if [ ! $docker_return -eq 0 ]; then
+  exit $docker_return
+fi
+
+# Wait for controller availability
+retries=1
+until $($wget -qO /dev/null 'http://localhost:8080'); do
+  sleep 0.1
+  ((retries -= 1 ))
+  if ((retries == 0)); then
+    echo "Controller did not become available withing 10 seconds"
+    exit 1
+  fi
+done
